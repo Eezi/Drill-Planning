f2f89ec78d193f466822282c49488ac8e8a844e9
Eezi <tomi.a.salo@outlook.com>
2022-07-15 20:02:28 +0300

    Added listview for workouts

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:flutter_quill/flutter_quill.dart';
 import 'package:markdown_app/components/drawer.dart';
 import 'package:markdown_app/components/bottomNavigation.dart';
 
@@ -11,11 +9,14 @@ class MyTabbedPage extends StatefulWidget {
 }
 
 class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderStateMixin {
-  String text = "";
+  late TextEditingController _controller;
+  List<String> exercises = [];
 
   @override
   void initState() {
     super.initState();
+
+    _controller = TextEditingController();
   }
 
  @override
@@ -29,8 +30,10 @@ class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderSt
       drawer: DrawerMenu(),
       bottomNavigationBar: BottomMenu(),
       floatingActionButton:FloatingActionButton( //Floating action button on Scaffold
-          onPressed: (){
-              //code to execute on button press
+          onPressed: () async{
+             final name = await openDialog();
+             if (name == null || name.isEmpty) return;
+             setState(() => this.exercises.add(name));
           },
           child: Icon(Icons.send), //icon inside button
       ),
@@ -41,8 +44,40 @@ class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderSt
         //title: const Text("Markdown Editor", style: TextStyle(color: Colors.white)),
       ),
       body: Container(
-            margin: EdgeInsets.all(20),
+        margin: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: exercises.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(exercises[index]),
+            );
+          },
         ),
+      ),
     );
   }
+
+Future openDialog() => showDialog(
+  context: context,
+  builder: (context) => AlertDialog(
+    title: Text("Harjoituksen nimi"),
+    content: TextField(
+      controller: _controller,
+      autofocus: true,
+      decoration: InputDecoration(hintText: 'Lisää harjoituksesi nimi' ),
+    ),
+    actions: [
+      TextButton(
+        child: Text("Lisää"),
+        onPressed: submit,
+      )
+    ]
+  )
+);
+
+  void submit() {
+    Navigator.of(context).pop(_controller.text);
+    _controller.clear();
+  }
 }
+
